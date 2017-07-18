42f901817b8e30576a50ace60508658e6f44017c
nadiaivannova <nadiaivannova@gmail.com>
2017-07-18 15:29:43 -0400

    added form and tested to see if it works

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,19 @@
 class PicturesController < ApplicationController
-  
+
   def index
     @pictures = Picture.all
   end
 
-  def show
-   @picture = Picture.find(params[:id])
+ def new
+   @picture = Picture.new
  end
+
+ def create
+   render text: "Received POST request to '/pictures' with the data URL: #{params.inspect}"
+ end
+
+ def show
+  @picture = Picture.find(params[:id])
+end
 
 end
