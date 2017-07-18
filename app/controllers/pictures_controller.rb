4e5d2f1b3da9b61814f8db0c093b69befac3766c
nadiaivannova <nadiaivannova@gmail.com>
2017-07-18 16:02:53 -0400

    added edit feture for each picture

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -27,6 +27,24 @@ class PicturesController < ApplicationController
 
  def show
   @picture = Picture.find(params[:id])
-end
+  end
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   get 'pictures/new' => 'pictures#new'
 
   get 'pictures/:id' => 'pictures#show'
+
+  get 'pictures/:id/edit' => "pictures#edit"
+  patch 'pictures/:id' => "pictures#update"
+
 end
 
 Rails.application.routes.draw do
